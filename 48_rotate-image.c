e015e7391575c27e4f8efb81b3025808d4c9f2ba
nbfhscl <nbfhscl@hotmail.com>
2022-04-16 23:56:59 +0800

    comment for matrix rotate

--- 48_rotate-image.c
+++ 48_rotate-image.c
@@ -2,6 +2,14 @@
  * 中等(medium)
  * 数组(array)
  * 矩阵(matrix)
+ *
+ * clockwise 90 degree by transpose and left right symmetric transformation
+ * 1 2    -    1 3   -   3 1
+ * 3 4    -    2 4   -   4 2 
+ * counter clockwise 90 degree by transpose and top bottom symmetric
+ * transformation
+ *                   -   2 4
+ *                   -   1 3
  */
 
 void rotate(int** matrix, int matrixSize, int* matrixColSize){
