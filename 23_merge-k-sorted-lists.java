c7eed888022b67ae1c296610ba8adb0cfc83d85f
songchenliang <songchenliang@supos.com>
2022-05-17 11:13:03 +0800

    merge k lists divide and conquer and priority queue

--- 23_merge-k-sorted-lists.java
+++ 23_merge-k-sorted-lists.java
@@ -1,11 +1,14 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  * 给你一个链表数组，每个链表都已经按升序排列。
  * 请你将所有链表合并到一个升序链表中，返回合并后的链表。
  *
  * 困难(hard)
  * 1. 归并merge
- * 2. todo: 分治. O(n*logk), O(1)
- * 3. todo: 优先级队列，priority queue. O(n*logk), O(1)
+ * 2. 分治. O(n*logk), O(1)
+ * 3. 优先级队列，priority queue. O(n*logk), O(1)
  */
 class Solution {
     // O(kn), O(1)
@@ -29,12 +32,58 @@ class Solution {
         }
         return ret.next;
     }
+    public ListNode mergeKLists_priority_queue(ListNode[] lists) {
+        if (lists.length == 0) {
+            return null;
+        }
+        ListNode ret = new ListNode();
+        ListNode head = ret;
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(
+                lists.length,
+                new Comparator<ListNode>() {
+                    public int compare(ListNode a, ListNode b) {
+                        return a.val - b.val;
+                    }
+                }
+        ); 
+        for (int i=0;i<lists.length;i++) {
+            if (lists[i] != null) {
+                pq.add(lists[i]);
+            }
+        }
+        for (;!pq.isEmpty();) {
+            head.next = pq.poll();
+            head = head.next;
+            if (head.next != null) {
+                pq.add(head.next);
+            }
+        }
+        return ret.next;
+    }
+
+    public ListNode mergeKLists_divide_and_conqure(ListNode[] lists) {
+        for (int k=lists.length;k>1;k = k/2 + k%2) {
+            for (int i=0, j=k-1;i<j;i++,j--) {
+                lists[i] = ListNode.merge(lists[i], lists[j], null);
+            }
+        }
+        return lists[0];
+    }
     public static void main(String[] args) {
         Solution sl = new Solution();
-        assert sl.mergeKLists_merge(new ListNode[]{
-                ListNode.getList(new int[]{3,4,9}),
+        ListNode.assertEqual(sl.mergeKLists_merge(new ListNode[]{
+            ListNode.getList(new int[]{3,4,9}),
                 ListNode.getList(new int[]{1,2,5})
-        }).equals(ListNode.getList(new int[]{1,2,3,4,5,9}));
+        }), ListNode.getList(new int[]{1,2,3,4,5,9}));
+        ListNode.assertEqual(sl.mergeKLists_priority_queue(new ListNode[]{
+            ListNode.getList(new int[]{3,4,9}),
+                ListNode.getList(new int[]{1,2,5})
+        }), ListNode.getList(new int[]{1,2,3,4,5,9}));
+        ListNode.assertEqual(sl.mergeKLists_divide_and_conqure(new ListNode[]{
+            ListNode.getList(new int[]{3,4,9}),
+                ListNode.getList(new int[]{1,2,5}),
+                ListNode.from(new int[]{0,3,10})
+        }), ListNode.getList(new int[]{0,1,2,3,3,4,5,9,10}));
     }
 }
 
--- ListNode.java
+++ ListNode.java
@@ -1,3 +1,5 @@
+import java.util.Comparator;
+
 public class ListNode {
     int val;
     ListNode next;
@@ -12,5 +14,48 @@ public class ListNode {
             head = head.next;
         }
         return ret.next;
+    }
+    public static ListNode from(int[] nodes) {
+        return getList(nodes);
+    }
+    public static void assertEqual(ListNode a, ListNode b) {
+        for (;a!=null && b!=null && a.val == b.val; a=a.next, b=b.next);
+        assert a==null && b==null;
+    }
+    public static ListNode merge(ListNode a, ListNode b, Comparator<ListNode> comp) {
+        if (comp == null) {
+            comp = new Comparator<ListNode>() {
+                public int compare(ListNode a, ListNode b) {
+                    return a.val - b.val;
+                }
+            };
+        }
+        ListNode ret = new ListNode();
+        ListNode head = ret;
+        for (; ;) {
+            if (a!=null && b!=null) {
+                if (comp.compare(a,b) < 0) {
+                    head.next = a;
+                    head = head.next;
+                    a = a.next;
+                } else {
+                    head.next = b;
+                    head = head.next;
+                    b = b.next;
+                }
+            } else if (a!=null) {
+                head.next = a;
+                break;
+            } else if (b!=null) {
+                head.next = b;
+                break;
+            } else {
+                break;
+            }
+        }
+        return ret.next;
+    }
+    public static void main (String[] args) {
+        assertEqual(merge(from(new int[]{1,3,7}), from(new int[]{2,5,8}), null), from(new int[]{1,2,3,5,7,8}));
     }
 }
